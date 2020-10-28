c3fd35fba83eac097fb53eb31e63abe47b493017
HBeate <hauser_b@hotmail.com>
2020-10-28 23:59:19 +0100

    Supermarkt funktioniert

--- Supermarkt/Supermarkt.cs
+++ Supermarkt/Supermarkt.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -34,12 +35,52 @@ namespace Supermarkt
                 {
                     shoppingList.Add(item, amount);
                     updateShoppingList();
-                    addListToTextFile();
+                    createShoppingListTxt();
+                }
+                else
+                {
+                    //jetzt erst mal nur die Anzahl geändert - noch nicht addiert bzw. subtrahiert
+                    int result;
+
+                   shoppingList.TryGetValue(item, out result);
+                        amount += result;
+
+                        shoppingList[item] = amount;
+                    updateShoppingList();
+                    createShoppingListTxt();
                 }
             }
         }
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            var item = tbItem.Text;
+            var number = tbAmount.Text;
+            int amount = Int32.Parse(tbAmount.Text);
 
-        private void addListToTextFile()
+            if (!string.IsNullOrEmpty(item) && (!string.IsNullOrEmpty(number)))
+            {
+                int result;
+
+                shoppingList.TryGetValue(item, out result);
+                if (shoppingList.ContainsKey(item) && (result >= amount))
+                {
+                    amount = result - amount;
+                    if (amount > 0)
+                    {
+                        shoppingList[item] = amount;
+                        updateShoppingList();
+                        createShoppingListTxt();
+                    }
+                    else
+                    {
+                        shoppingList.Remove(item);
+                        updateShoppingList();
+                        createShoppingListTxt();
+                    }
+                }
+            }
+        }
+        private void createShoppingListTxt()
         {
             using (var writer = new StreamWriter(dictionaryPath))
             {
@@ -60,9 +101,6 @@ namespace Supermarkt
             }
             lBoxItems.DataSource = list.ToList();
         }
-        private void btnRemove_Click(object sender, EventArgs e)
-        {
 
-        }
     }
 }
